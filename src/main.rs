fa1e859fa2cfc6c6236a683501ef0d7eb7773e66
Sébastien PRUNIER <sebastien.prunier@etu.univ-lehavre.fr>
2025-12-06 20:38:23 +0100

    ex3 1

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
 mod ex1;
 mod ex2;
+mod ex3;
 
 fn main() {
-    ex1::part1::run();
-    ex1::part2::run();
-    print!("\n");
+    // ex1::part1::run();
+    // ex1::part2::run();
+    // print!("\n");
 
-    ex2::part1::run();
-    ex2::part2::run();
+    // ex2::part1::run();
+    // ex2::part2::run();
+    // print!("\n");
+
+    ex3::part1::run();
 }
